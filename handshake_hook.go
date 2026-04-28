def43dde7444d1120a26f8cc97920f1f5b86b671
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-28 13:55:52 +0800

    implement onClientHelloMessage and onServerHelloMessage, update message.

--- handshake_hook.go
+++ handshake_hook.go
@@ -1,25 +1,55 @@
 package utls
 
 type ClientHelloMessage struct {
+	Random    []byte
+	SessionID []byte
+	ALPNProto []string
 }
 
 type ServerHelloMessage struct {
+	Random    []byte
+	SessionID []byte
+	ALPNProto string
 }
 
 func (c *Conn) onClientHelloMessage(hello *clientHelloMsg) error {
-	// clone client hello message
-
 	if c.config.OnClientHelloMessage == nil {
 		return nil
 	}
-	return c.config.OnClientHelloMessage()
+	// clone client hello message
+	msg := &ClientHelloMessage{
+		Random:    hello.random,
+		SessionID: hello.sessionId,
+		ALPNProto: hello.alpnProtocols,
+	}
+	err := c.config.OnClientHelloMessage(msg)
+	if err != nil {
+		return err
+	}
+	// adjust the original hello message
+	hello.random = msg.Random
+	hello.sessionId = msg.SessionID
+	hello.alpnProtocols = msg.ALPNProto
+	return nil
 }
 
 func (c *Conn) onServerHelloMessage(hello *serverHelloMsg) error {
-	// clone server hello message
-
 	if c.config.OnServerHelloMessage == nil {
 		return nil
 	}
-	return c.config.OnServerHelloMessage()
+	// clone server hello message
+	msg := &ServerHelloMessage{
+		Random:    hello.random,
+		SessionID: hello.sessionId,
+		ALPNProto: hello.alpnProtocol,
+	}
+	err := c.config.OnServerHelloMessage(msg)
+	if err != nil {
+		return err
+	}
+	// adjust the original hello message
+	hello.random = msg.Random
+	hello.sessionId = msg.SessionID
+	hello.alpnProtocol = msg.ALPNProto
+	return nil
 }
