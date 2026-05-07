d1834cc2541a9769db2220fb97b43308e5155ccf
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-07 13:28:24 +0800

    update tests about on hello message that add sub tests for different API.

--- handshake_hook_test.go
+++ handshake_hook_test.go
@@ -8,17 +8,19 @@ import (
 )
 
 func TestOnClientHelloMessage(t *testing.T) {
+	hook := func(hello *ClientHelloMessage) error {
+		secret := make([]byte, 32)
+		secret[0] = 0xFF
+		if bytes.Equal(hello.Random, secret) {
+			hello.ALPNProto = []string{"http/1.1"}
+		}
+		return nil
+	}
+
 	serverCfg := &Config{
-		Certificates: testConfig.Clone().Certificates,
-		NextProtos:   []string{"h2", "http/1.1"},
-		OnClientHelloMessage: func(hello *ClientHelloMessage) error {
-			secret := make([]byte, 32)
-			secret[0] = 0xFF
-			if bytes.Equal(hello.Random, secret) {
-				hello.ALPNProto = []string{"http/1.1"}
-			}
-			return nil
-		},
+		Certificates:         testConfig.Clone().Certificates,
+		NextProtos:           []string{"h2", "http/1.1"},
+		OnClientHelloMessage: hook,
 	}
 
 	listener, err := Listen("tcp", "127.0.0.1:0", serverCfg)
@@ -46,18 +48,34 @@ func TestOnClientHelloMessage(t *testing.T) {
 	clientCfg.Random = make([]byte, 32)
 	clientCfg.Random[0] = 0xFF
 
-	raw, err := net.Dial("tcp", listener.Addr().String())
-	testCheckError(t, err)
-	conn := UClient(raw, clientCfg, HelloFirefox_Auto)
+	t.Run("Dial", func(t *testing.T) {
+		conn, err := Dial("tcp", listener.Addr().String(), clientCfg.Clone())
+		testCheckError(t, err)
 
-	err = conn.Handshake()
-	testCheckError(t, err)
-	if conn.ConnectionState().NegotiatedProtocol != "http/1.1" {
-		t.Fatal("NegotiatedProtocol should be http/1.1")
-	}
+		err = conn.Handshake()
+		testCheckError(t, err)
+		if conn.ConnectionState().NegotiatedProtocol != "http/1.1" {
+			t.Fatal("NegotiatedProtocol should be http/1.1")
+		}
 
-	err = conn.Close()
-	testCheckError(t, err)
+		err = conn.Close()
+		testCheckError(t, err)
+	})
+
+	t.Run("UClient", func(t *testing.T) {
+		raw, err := net.Dial("tcp", listener.Addr().String())
+		testCheckError(t, err)
+		conn := UClient(raw, clientCfg.Clone(), HelloFirefox_Auto)
+
+		err = conn.Handshake()
+		testCheckError(t, err)
+		if conn.ConnectionState().NegotiatedProtocol != "http/1.1" {
+			t.Fatal("NegotiatedProtocol should be http/1.1")
+		}
+
+		err = conn.Close()
+		testCheckError(t, err)
+	})
 }
 
 func TestOnServerHelloMessage(t *testing.T) {
@@ -82,30 +100,47 @@ func TestOnServerHelloMessage(t *testing.T) {
 		testCheckError(t, err)
 	}()
 
+	hook := func(hello *ServerHelloMessage) error {
+		t.Log("random:", hello.Random)
+		t.Log("session id:", hello.SessionID)
+		t.Log("protocol:", hello.ALPNProto)
+		return nil
+	}
 	clientCfg := &Config{
-		NextProtos:         []string{"h2", "http/1.1"},
-		RootCAs:            x509.NewCertPool(),
-		InsecureSkipVerify: true,
-		OnServerHelloMessage: func(hello *ServerHelloMessage) error {
-			t.Log("random:", hello.Random)
-			t.Log("session id:", hello.SessionID)
-			t.Log("protocol:", hello.ALPNProto)
-			return nil
-		},
+		NextProtos:           []string{"h2", "http/1.1"},
+		RootCAs:              x509.NewCertPool(),
+		InsecureSkipVerify:   true,
+		OnServerHelloMessage: hook,
 	}
 
-	raw, err := net.Dial("tcp", listener.Addr().String())
-	testCheckError(t, err)
-	conn := UClient(raw, clientCfg, HelloFirefox_Auto)
+	t.Run("Dial", func(t *testing.T) {
+		conn, err := Dial("tcp", listener.Addr().String(), clientCfg.Clone())
+		testCheckError(t, err)
 
-	err = conn.Handshake()
-	testCheckError(t, err)
-	if conn.ConnectionState().NegotiatedProtocol != "h2" {
-		t.Fatal("NegotiatedProtocol should be h2")
-	}
+		err = conn.Handshake()
+		testCheckError(t, err)
+		if conn.ConnectionState().NegotiatedProtocol != "h2" {
+			t.Fatal("NegotiatedProtocol should be h2")
+		}
 
-	err = conn.Close()
-	testCheckError(t, err)
+		err = conn.Close()
+		testCheckError(t, err)
+	})
+
+	t.Run("UClient", func(t *testing.T) {
+		raw, err := net.Dial("tcp", listener.Addr().String())
+		testCheckError(t, err)
+		conn := UClient(raw, clientCfg, HelloFirefox_Auto)
+
+		err = conn.Handshake()
+		testCheckError(t, err)
+		if conn.ConnectionState().NegotiatedProtocol != "h2" {
+			t.Fatal("NegotiatedProtocol should be h2")
+		}
+
+		err = conn.Close()
+		testCheckError(t, err)
+	})
 }
 
 func testCheckError(t *testing.T, err error) {
